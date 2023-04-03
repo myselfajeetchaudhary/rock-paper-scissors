d2e8438daa504e2f1891fe7b1b9e3bd97c8173ea
Ajeet Chaudhary <masterajeet1@gmail.com>
2023-04-03 11:41:36 +0530

    Fix playRound() bug

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let userChoice = prompt('Choose between (Rock, Paper, Scissors)');
+let playerSelection = userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLocaleLowerCase();
+let computerSelection = getComputerChoice();
+
+// Computer's Random Choice of Rock, Paper, Scissors
 function getComputerChoice() {
-    return 
+    let gameText = ['Rock', 'Paper', 'Scissors'];
+    let randomNumber = [Math.floor(Math.random() * gameText.length)];
+
+    return gameText[randomNumber];
 }
+
+function playRound() {
+    return (playerSelection === computerSelection) ? 'You Won!' : 'You Lose!';
+}
+
+console.log(playRound());
